0db1fec880d7eabb78f65354626723cb83f64cb2
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-07-03 19:43:10 -0400

    handle errors

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/project/:id', (req,res)=>{
     if(Object.keys(projectDetails).length > 0 ){
         res.render('project',projectDetails);
     }else{
-    res.redirect(`/projects-does-not-exist-with-id/${id}`)
+    res.redirect(`/projects/${id}`)
     }
 });
 app.use((req,res,next)=>{
@@ -52,14 +52,29 @@ app.use((req,res,next)=>{
 
 })
 app.use((err,req, res,next) => {
+    // reconstruct error message.
+    const notFound = 404;
+    const internalError = 500; 
+    const catchAllError = new Error()
+
     if(!err.status){
-        logMessage(" Error ")
-        console.error(err);
-        err.status = 500;
+        catchAllError.message = "Internal Server Error :(";
+        catchAllError.status = internalError;
+        catchAllError.stack = err.stack;
+    } else {
+        catchAllError.message = err.message;
+        catchAllError.stack = err.stack;
+        catchAllError.status = notFound;
     }
-    res.locals.error = err
-    res.status(err.status);
-    res.send(err);
+    res.locals.error = catchAllError;
+    res.status(catchAllError.status);
+    
+    console.log('+ + + error + + +');
+    console.log(catchAllError);
+
+    catchAllError.status === notFound? 
+                res.render('page-not-found',catchAllError) : 
+                res.render('error',catchAllError);
   });
 /**
  * TODO: Consider adding a target attribute set to _blank on the a tags for the live links to your projects so that they open in a new window.
